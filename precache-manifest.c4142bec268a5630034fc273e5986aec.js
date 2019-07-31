13003d2ddfdff325afa57ab4a527eea1040259cb
ashksin121 <ashksin121@gmail.com>
2019-08-01 01:17:26 +0530

    Updates

---
+++ precache-manifest.c4142bec268a5630034fc273e5986aec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2e2d78f9be9231f51267ce7f76ae82",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "855394c5d6010575ff25",
+    "url": "/Todo-App/static/css/main.9b7be588.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/Todo-App/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "855394c5d6010575ff25",
+    "url": "/Todo-App/static/js/main.e19d1dd7.chunk.js"
+  },
+  {
+    "revision": "2acd21dc4a6582beab29",
+    "url": "/Todo-App/static/js/runtime~main.3fb21290.js"
+  },
+  {
+    "revision": "ffb835de3ca328262309d5cea09c4a1c",
+    "url": "/Todo-App/static/media/app-logo.ffb835de.png"
+  }
+]);
